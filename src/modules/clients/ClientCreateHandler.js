95a0781517e20455690f141fc79f4520b55743a8
wendy <wendyvanesavera@gmail.com>
2026-06-17 17:09:15 -0500

    Fix: En la vista de creacion de cliente se modifico el minimo de digitos a ingresar en el campo document_number y campo phone para que se ingresen solo 10 digitos

--- src/modules/clients/ClientCreateHandler.js
+++ src/modules/clients/ClientCreateHandler.js
@@ -82,7 +82,8 @@ export const ClientCreateHandler = async () => {
         const rules = {
             document_number: {
                 required: true,
-                minLength: 5,
+                minLength: 7,
+                maxLength: 10,
                 pattern: /^[0-9]+$/,
                 patternMessage: 'El documento debe contener exclusivamente números.'
             },
@@ -94,9 +95,8 @@ export const ClientCreateHandler = async () => {
             },
             phone: {
                 required: true,
-                minLength: 7,
-                pattern: /^[0-9+\-\s]+$/,
-                patternMessage: 'El teléfono contiene caracteres no permitidos.'
+                pattern: /^[0-9]{10}$/,
+                patternMessage: 'El teléfono solo debe contener 10 digitos.'
             }
         };
 
