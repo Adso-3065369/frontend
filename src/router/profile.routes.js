0dffae8886029494c0e48d753ff0116c80190f2a
sfbtm <dilansanv022@gmail.com>
2026-06-17 15:33:59 -0500

    feat: ruta creada para vista profile /profile

---
+++ src/router/profile.routes.js
@@ -0,0 +1,36 @@
+/**
+ * @file profile.routes.js
+ * @description Sub-enrutador para el perfil de usuario.
+ */
+
+
+// Importacion de vista y handler del modulo de perfil
+import {
+    ProfileView,
+    ProfileHandler
+} from "@/modules/profile";
+
+import {
+    NotFoundView
+} from "@/views";
+ 
+// importacion del layout privado 
+import { PrivateLayout } from '@/layouts';
+
+
+
+/**
+ * Sub-arreglo de rutas para la vista de perfil 
+ * Protegidas bajo autenticación estricta.
+ */
+
+export const profileRoutes = [
+        { 
+            path: '#/profile', 
+            view: ProfileView, 
+            init: ProfileHandler,
+            requiresAuth: true,
+            layout: PrivateLayout,
+            // permissions: ['products.index']
+        },
+]
