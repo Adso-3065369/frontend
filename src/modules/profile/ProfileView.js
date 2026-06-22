258c1467bf6d7f4a3d5672d59bd34a36197d0289
sfbtm <dilansanv022@gmail.com>
2026-06-22 18:06:57 -0500

    feat: card de informacion de perfil

--- src/modules/profile/ProfileView.js
+++ src/modules/profile/ProfileView.js
@@ -1,10 +1,26 @@
-import { Link, Badge } from '@/components/ui';
+import { Link, Badge, Card } from '@/components/ui';
 import { RenderIf } from '@/utils';
 
 /**
  * @file UserListView.js
  * @description Interfaz de gestión de usuarios ajustada al patrón de componentes dinámicos.
  */
+
+
+const cardContent = `
+                <div class="flex items-center gap-5">
+                    <div class="bg-black w-20 h-20 flex justify-center items-center rounded-full">U</div>
+                    <div class="flex-row items-center">
+                        <p class="text-text-primary">Tu persona</p>
+                        <p class="text-text-secondary">Tu persona@gmail.com</p>
+                    </div>
+                </div>
+                ${Badge({
+                    text: "xdddddd",
+                    variant: 'info',
+                    className: 'w-20 h-10 rounded-xl border-none'
+                })}`
+
 export const ProfileView = async () => {
     return `
         <div class="p-6 space-y-6">
@@ -15,15 +31,13 @@ export const ProfileView = async () => {
                 </div>
             </div>
 
-            <div id="user-card" class="flex gap-5 bg-orange-700 app-card overflow-hidden h-50 w-100 p-10">
-                <div class="bg-black w-10 h-10 flex justify-center items-center rounded-full">U</div>
-                <div class="flex-row items-center">
-                    <p class="text-text-primary">Tu persona</p>
-                    <p class="text-text-secondary">Tu persona@gmail.com</p>
-                </div>
-                ${Badge({
-                    text: "hola"
-                })}
+            ${Card({
+                children: cardContent,
+                bodyClass: 'flex-row gap-5 items-center px-5 py-10 justify-between'
+            })}
+
+            <div id="user-card" class="flex justify-between items-center bg-[var(--bg-surface)] rounded-md app-card overflow-hidden h-50 w-150 p-10">
+                
             </div>
             <div id="users-table-container" class="app-card overflow-hidden">
                 <div class="px-6 py-12 text-center text-text-secondary italic">
