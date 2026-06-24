23568e740cdbba60d3035beaab0c4e81e0b530ab
sfbtm <dilansanv022@gmail.com>
2026-06-24 16:57:58 -0500

    docs: profileCard.js documentada con comentarios

--- src/modules/profile/components/profileCard.js
+++ src/modules/profile/components/profileCard.js
@@ -1,27 +1,33 @@
-import { Link, Badge, Card } from '@/components/ui';
+/**
+ * @file profileCard.js
+ * @description Componente atómico contenedor. Centraliza la estructura base de las superficies del sistema.
+ * @param {Object} props - Diccionario de parámetros de configuración del componente.
+ * @param {string} [props.id=''] - Id que se le puede dar a la Card de modo que se le puedan meter estilos dinamicos
+ * @param {string} [props.name=''] - El nombre del usuario
+ * @param {string} [props.role=''] - El rol del usuario
+ * @returns {string} Cadena de texto con el marcado HTML compilado (`<div class="...">...</div>`) listo para su inyección en el DOM.
+ */
+
+import { Badge } from '@/components/ui';
 
 
-export const profileCard = () => {
-    const cardContent = 
-    
-    `
+export const profileCard = ({
+    name = '',
+    email = '',
+    role = ''
+}) => {
+    return `
     <div class="flex items-center gap-5">
-    <div class="bg-black w-20 h-20 flex justify-center items-center rounded-full">U</div>
-    <div class="flex-row items-center">
-    <p class="text-text-primary">Tu persona</p>
-    <p class="text-text-secondary">Tu persona@gmail.com</p>
-    </div>
+        <div class="bg-black w-20 h-20 flex justify-center items-center rounded-full text-xl text-(--primary-dark) font-bold">${name[0].toUpperCase()}</div>
+        <div class="flex-row items-center">
+            <p class="text-text-primary">${name}</p>
+            <p class="text-text-secondary">${email}</p>
+        </div>
     </div>
     ${Badge({
-        text: "xdddddd",
+        text: `${role}`,
         variant: 'info',
         className: 'w-20 h-10 rounded-xl border-none'
     })}`
-    
 
-    
-    return Card({
-                    children: cardContent,
-                    bodyClass: 'flex-row gap-5 items-center px-5 py-10 justify-between'
-                })
 }
