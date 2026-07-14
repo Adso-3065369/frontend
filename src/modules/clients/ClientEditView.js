b9e999b9c16320105554b4e75077b163de62684b
Jhonatan <lehericijhonathandavid@gmail.com>
2026-07-14 15:35:12 -0500

    Fix/Se realizo la correcion del label agregando 'HtmlFor' al modulo de clientes

--- src/modules/clients/ClientEditView.js
+++ src/modules/clients/ClientEditView.js
@@ -34,6 +34,7 @@ export const ClientEditView = async () => {
                         <div class="space-y-2">
                             ${Label({
                                 text:'Número de Documento',
+                                htmlFor:'document_number'  
                             })}
                             ${Input({
                                 type: 'text',
@@ -47,6 +48,7 @@ export const ClientEditView = async () => {
                         <div class="space-y-2">
                             ${Label({
                                 text:'Nombre Completo / Razón Social',
+                                htmlFor:'name' 
                             })}
                             ${Input({
                                 type: 'text',
@@ -60,6 +62,7 @@ export const ClientEditView = async () => {
                         <div class="space-y-2">
                             ${Label({
                                 text:'Correo Electronico',
+                                htmlFor:'email' 
                             })}
                             ${Input({
                                 type: 'text',
@@ -73,6 +76,7 @@ export const ClientEditView = async () => {
                         <div class="space-y-2">
                             ${Label({
                                 text:'Telefono de Contacto',
+                                htmlFor: 'phone'
                             })}
                             ${Input({
                                 type: 'text',
