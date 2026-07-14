6dd57c68d7cc69835fa73e9b76a70867bca53b89
EileenMendoza18 <eileenjml4816@gmail.com>
2026-07-14 06:44:25 -0500

    Fix: Implementar validacion solo en edicion de categoria

--- src/modules/categories/CategoryCreateHandler.js
+++ src/modules/categories/CategoryCreateHandler.js
@@ -71,9 +71,7 @@ export const CategoryCreateHandler = async () => {
             categoryName: {
                 required: true,
                 minLength: 3,
-                message: 'El nombre de la categoría es obligatorio y debe tener al menos 3 caracteres.',
-                pattern: /^(?=.*[a-zA-ZáéíóúÁÉÍÓÚñÑ]).+$/,
-                patternMessage: 'El nombre de la categoría no puede contener solo números.'
+                message: 'El nombre de la categoría es obligatorio y debe tener al menos 3 caracteres.'
             }
         };
 
